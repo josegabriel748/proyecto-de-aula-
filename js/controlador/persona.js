06e3ffb801cfdf4af006aa1b4169d8e04db2e84c
josegabriel748 <josegmartinez@unicesar.edu.co>
2024-06-11 13:35:46 -0500

    Delete js directory

--- js/controlador/persona.js
+++
@@ -1,9 +0,0 @@
-class Person{
-    constructor(nombre,cedula, correo, telefono,contrasenhia){
-        this.nombre = nombre;
-        this.cedula = cedula;
-        this.correo = correo;
-        this.telefono = telefono;
-        this.contrasenhia = contrasenhia;
-    }
-}
